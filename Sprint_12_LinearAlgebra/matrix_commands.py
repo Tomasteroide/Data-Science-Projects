f8e5c9e194c00e701a3d256ac62b0baf6ad59b43
Tomasteroide <nelson.avilescaputo@gmail.com>
2025-02-09 19:52:03 -0300

    creation of the sprint 12

---
+++ Sprint_12_LinearAlgebra/matrix_commands.py
@@ -0,0 +1,14 @@
+# Matrix creation
+
+import numpy as np
+matrix = np.array([[3, 14, 159],[-2, 7, 183]])
+print(matrix)
+
+# DF to matrix
+import pandas as pd
+
+monday_df = pd.DataFrame({'Minutos': [10, 3, 15, 27, 7], 
+                          'Mensajes': [2, 5, 3, 0, 1], 
+                          'Megabytes': [72, 111, 50, 76, 85]})
+ 
+monday = monday_df.values
