5d008e96c801a5c3ca8bee2bfc81395134c527de
Tomas Aviles <tomas.aviles@kredito.cl>
2025-02-12 16:00:38 -0300

    advanced on S12

--- Sprint_12_LinearAlgebra/matrix_commands.py
+++ Sprint_12_LinearAlgebra/matrix_commands.py
@@ -11,4 +11,11 @@ monday_df = pd.DataFrame({'Minutos': [10, 3, 15, 27, 7],
                           'Mensajes': [2, 5, 3, 0, 1], 
                           'Megabytes': [72, 111, 50, 76, 85]})
  
-monday = monday_df.values
+monday = monday_df.values #.values creates de matrix
+
+print(monday)
+print(monday[2]) # third row
+print(monday[:,2]) # third column
+
+# Matrix operations
+
